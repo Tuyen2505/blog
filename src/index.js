b805971bf3211f8b31430963c03facad0d21f2ab
tuyen <pmtzzp2505@gmail.com>
2024-08-15 21:29:18 +0700

    abc

--- src/index.js
+++ src/index.js
@@ -4,23 +4,28 @@ const path = require('path')
 const app = express()
 const port = 3000
 const handlebars = require('express-handlebars')
+const route = require('./routes')
+
 
 app.use(express.static(path.join(__dirname, 'public')))
+app.use(express.urlencoded({
+  extended: true,
+}));
+app.use(express.json());
 
 app.use(morgan('combined'))
+
 // Template Engine
 app.engine('hbs', handlebars.engine({extname: '.hbs'}));
 app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname, 'resources/views'));
-// HTTP Logger
-app.get('/', (req, res) => {
-  res.render('home');
-})
 
-app.get('/news', (req, res) => {
-  res.render('news');
-})
+
+// Route init
+route(app);
+
+
+
 app.listen(port, () => {
   console.log(`Example app listening on port http://localhost:${port}`)
 })
-// Bản mới nhất mà vẫn chạy được, mời các bạn tham khảo
